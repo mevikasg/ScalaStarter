7a4e79dd60d1da7f96425d7a1e7f0d13169e3004
Vikas Gupta <mevikasg@gmail.com>
2016-04-18 21:47:03 -0400

    Initial commit

---
+++ src/com/vikasg/scalastarter/java2scala/Java2Scala.scala
@@ -0,0 +1,16 @@
+package com.vikasg.scalastarter.java2scala
+
+/**
+  * Created by vikasg on 4/18/2016.
+  */
+object Java2Scala {
+
+  def main(args:Array[String]) {
+
+    println("Hello Scala!!!")
+
+
+
+  }
+
+}
